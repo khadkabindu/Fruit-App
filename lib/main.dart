58f633839c5a64b62b5f22f0c137ef1fa4dda3de
bindu <bindu@Rajeshs-MacBook-Pro.local>
2020-05-20 17:40:58 +0545

    added favourite button

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,7 @@ import 'counter.dart';
 import 'image_carousel.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => FirstScreen(),
-    },
-  ));
+  runApp(FirstScreen());
 }
 
 class FirstScreen extends StatefulWidget {
@@ -21,6 +16,14 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreen extends State<FirstScreen> {
+  bool _isFavorited = true;
+
+  void _toggleFavorite() {
+    setState(() {
+      _isFavorited = !_isFavorited;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,28 +36,22 @@ class _FirstScreen extends State<FirstScreen> {
               SizedBox(
                 width: 5.0,
               ),
-              Container(
-                child: IconButton(
-                  color: Colors.black,
-                  icon: const Icon(
-                    Icons.arrow_back_ios,
-                  ),
-                  tooltip: 'Next page',
-                  onPressed: () {},
+              IconButton(
+                color: Colors.white,
+                icon: const Icon(
+                  Icons.arrow_back,
+                  size: 40.0,
                 ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              )
+                onPressed: () {},
+              ),
             ],
           ),
           actions: <Widget>[
             IconButton(
               icon: const Icon(
-                Icons.filter_list,
+                Icons.shopping_cart,
                 color: Colors.white,
-                size: 50.0,
+                size: 40.0,
               ),
               onPressed: () {},
             ),
@@ -64,112 +61,122 @@ class _FirstScreen extends State<FirstScreen> {
           ],
         ),
         backgroundColor: Colors.amber[200],
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.end,
+        body: ListView(
           children: <Widget>[
-            CarouselWithIndicatorDemo(),
-            SizedBox(
-              height: 50.0,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: new BorderRadius.only(
-                    topLeft: const Radius.circular(50.0),
-                    topRight: const Radius.circular(50.0),
-                  )),
-              height: 500.0,
-              width: 500.0,
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text(
-                        'Mango',
-                        style: TextStyle(
-                            fontSize: 30.0, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text('1 each'),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      CounterDesign(),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      Text(
-                        'Product Description',
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      Text('Mangoes tropical taste has a universal appeal. '
-                          'The meat has a sweatness of something that is great.'
-                          'Mangoes tropical taste has a universal appeal. '
-                          'The meat has a sweatness of something that is great.'),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      Row(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                CarouselWithIndicatorDemo(),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: new BorderRadius.only(
+                        topLeft: const Radius.circular(50.0),
+                        topRight: const Radius.circular(50.0),
+                      )),
+                  height: 500.0,
+                  width: 500.0,
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          ButtonTheme(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                              side: BorderSide(color: Colors.amber[200]),
-                            ),
-                            height: 70.0,
-                            child: RaisedButton(
-                              color: Colors.white,
-                              elevation: 0.0,
-                              child: Icon(
-                                Icons.favorite_border,
-                                color: Colors.amber[200],
-                                size: 40.0,
-                              ),
-                              onPressed: () {},
-                            ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            'Mango',
+                            style: TextStyle(
+                                fontSize: 30.0, fontWeight: FontWeight.bold),
                           ),
                           SizedBox(
-                            width: 20.0,
+                            height: 10.0,
                           ),
-                          ButtonTheme(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0)),
-                            height: 70.0,
-                            minWidth: 260.0,
-                            child: RaisedButton(
-                              elevation: 0.0,
-                              color: Colors.amber[300],
-                              onPressed: () {
-                                setState(() {
-                                  Icon(Icons.favorite);
-                                });
-                              },
-                              child: Text(
-                                'Add to cart',
-                                style: TextStyle(fontWeight: FontWeight.bold),
+                          Text('1 each'),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          CounterDesign(),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          Text(
+                            'Product Description',
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'A mango is a type of fruit. '
+                            'The mango tree is native to South Asia, '
+                            'from where it has been taken to become one of the most widely cultivated fruits in the tropics.'
+                            'It is harvested in the month of march (summer season) till the end of May.',
+                            style:
+                                TextStyle(letterSpacing: 2.0, fontSize: 15.0),
+                          ),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              ButtonTheme(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  side: BorderSide(color: Colors.amber[200]),
+                                ),
+                                height: 70.0,
+                                child: RaisedButton(
+                                  color: Colors.white,
+                                  elevation: 0.0,
+                                  child: IconButton(
+                                      icon: _isFavorited
+                                          ? Icon(
+                                              Icons.favorite_border,
+                                              color: Colors.orange,
+                                            )
+                                          : Icon(
+                                              Icons.favorite,
+                                              color: Colors.orange,
+                                            )),
+                                  onPressed: _toggleFavorite,
+                                ),
                               ),
-                            ),
-                          )
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              ButtonTheme(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0)),
+                                height: 70.0,
+                                minWidth: 260.0,
+                                child: RaisedButton(
+                                  elevation: 0.0,
+                                  color: Colors.amber[300],
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Add to cart',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
